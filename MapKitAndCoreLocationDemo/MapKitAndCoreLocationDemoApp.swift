51410b2302243e93ca2b1622dd8ed0a15d992a79
crazygit <lianglin999@gmail.com>
2023-08-01 11:43:23 +0800

    Initial Commit

---
+++ MapKitAndCoreLocationDemo/MapKitAndCoreLocationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAndCoreLocationDemoApp.swift
+//  MapKitAndCoreLocationDemo
+//
+//  Created by Crazygit on 2023/8/1.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAndCoreLocationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
